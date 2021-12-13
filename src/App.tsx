5d83c517222437390ed9cd3c802275274514de35
culi <culitif@tuta.io>
2021-12-13 14:23:39 -0800

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello earth!</h1>
+      <p>
+        Earth day is April 22. Plant a tree, turn your lights off, learn indigenous names for native plants, and build earthen traditions.
+      </p>
+    </>
+  )
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
+    // publicPath: '/',
   },
   module: {
     rules: [
