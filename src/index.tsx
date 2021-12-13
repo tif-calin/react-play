71dda121dc3b5ffdd7d8787df8e9b16df1645712
culi <culitif@tuta.io>
2021-12-13 14:45:38 -0800

    move App to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello earth!</h1>
+      <p>
+        Earth day is April 22. Plant a tree, turn your lights off, learn indigenous names for native plants, and build earthen traditions.
+      </p>
+    </>
+  )
+};
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(<App />);
