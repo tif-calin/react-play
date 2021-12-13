260022620525dd5f654eb3f574e2ef5f068dd335
culi <culitif@tuta.io>
2021-12-13 13:14:10 -0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devServer: {
+    static: path.join(__dirname, './src'),
+    port: 3001,
+    hot: 'only',
+    compress: true,
+    open: true,
+  },
+  mode: 'development',
+};
