a7f16bfa54f4044b29e66509d1b56be735b22c7e
culi <culitif@tuta.io>
2021-12-14 23:28:35 -0800

    create home page

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,5 +1,18 @@
-const Home = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Home = () => {
+  return (
+    <>
+      <h1>Hello earth!</h1>
+      <p>
+        Earth day is April 22. Plant a tree, turn your lights off, learn indigenous names for native plants, and build earthen traditions.
+      </p>
+      <p>
+        <Link to="/rate">raterate</Link>
+      </p>
+    </>
+  )
 };
 
 export default Home;
