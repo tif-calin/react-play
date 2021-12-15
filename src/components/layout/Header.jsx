d1c7a54d569560226c2ab6544066b1f522403a3d
culi <culitif@tuta.io>
2021-12-14 23:28:06 -0800

    add link to home in header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="Header">
-      welcome
+      <Link to="/">welcome</Link>
     </header>
   );
 };
