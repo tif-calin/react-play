c047c7198ee56a4b80e7259a306d8a8216164614
culi <culitif@tuta.io>
2021-12-14 23:28:44 -0800

    create rate page

---
+++ src/views/rate/Rate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Rate = () => {
+  return (
+    <>
+      <h1>rate this joke</h1>
+      <p><strong>Can someone please tell me what the lowest rank in the military is?</strong></p>
+      <p>Every time I ask they say "it's private"!</p>
+    </>
+  );
+};
+
+export default Rate;
