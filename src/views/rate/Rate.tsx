2b3b54fa8ad84b687e449416a722242af9a77f42
culi <culitif@tuta.io>
2021-12-17 22:08:10 -0800

    raterate page

--- src/views/rate/Rate.tsx
+++ src/views/rate/Rate.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import RateField from '../../components/rate/RateField';
 
 const Rate = () => {
   return (
     <>
-      <h1>rate this joke</h1>
+      <PageTitle>RateRate</PageTitle>
       <p><strong>Can someone please tell me what the lowest rank in the military is?</strong></p>
       <p>Every time I ask they say "it's private"!</p>
+      <RateField />
     </>
   );
 };
