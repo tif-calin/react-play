f64a9323cb6a7ce1906c7123a3e194b0a9e4174c
culi <culitif@tuta.io>
2021-12-24 19:56:31 -0800

    hasSameValues accepts exceptions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,8 @@
 
-const hasSameValues = (a: {[key: string]: any}, b: {[key: string]: any}) => {
+const hasSameValues = (a: {[key: string]: any}, b: {[key: string]: any}, except: string[] = []) => {
   // does object a have all the values of object b?
-  for (let key in b) {
-    if (a[key] !== b[key]) return false;
-  }
-
-  return true;
+  if (except.length) return Object.keys(a).filter(key => !except.includes(key)).every(key => a[key] === b[key]);
+  else return Object.keys(a).every(key => a[key] === b[key]);
 };
 
 export { hasSameValues };
