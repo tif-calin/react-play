d2426e42807001dce932c71d6d331c91c9c46775
culi <culitif@tuta.io>
2021-12-27 20:16:43 -0800

    move all dat

--- src/views/catfood/Catfood.tsx
+++ src/views/catfood/Catfood.tsx
@@ -1,67 +1,25 @@
 import React from 'react';
 import PageTitle from '../../components/layout/PageTitle';
-import useLocalStorage from '../../hooks/useLocalStorage';
 import PriceObservationForm from './PriceObservationForm';
 import PriceObservationList from './PriceObservationList';
-
+import useCatfoodData from '../../hooks/useCatfood';
 import styles from './catfood.module.css';
-import { hasSameValues } from '../../utils/utils';
-
-type DataEntry = {
-  food: string,
-  weight: number,
-  price: number,
-  store: string,
-  place: string,
-  date: string
-};
-type Option = {
-  value: string,
-  store: string,
-  label?: string
-};
-
-const getOptions = (data: DataEntry[]): Option[] => {
-  const log = data.reduce((acc: {[key: string]: string}, cur) => {
-    const { store, place } = cur;
-
-    return acc[place] ? acc : { ...acc, [place]: store };
-  }, {});
-
-  return Object.keys(log).map(key => ({value: key, store: log[key]}));
-};
 
 const Catfood = () => {
-  const [data, setData] = useLocalStorage<DataEntry[]>('catfood_data', []);
-
-  const addEntry = React.useCallback((entry: DataEntry) => {
-    console.log(entry);
-    Object.entries(entry).forEach(([key, val]) => {
-      if (typeof val === 'string') {
-        entry[key] = val.trim();
-      }
-    });
-    console.log(entry);
-
-    if (data.some(obs => hasSameValues(obs, entry, ['price']))) console.log(`Duplicate entry: ${entry}`);
-    else setData(data => [...data, entry]);
-  }, [data]);
-
-  const removeEntry = React.useCallback((index: number) => {
-    setData(data => data.filter((_, i) => i !== index));
-  }, [data]);
-
-  const eraseAll = React.useCallback(() => setData([]), [data]);
+  const { data, stats, storeOptions, addEntry, addEntries, removeEntry, eraseAll } = useCatfoodData();
 
   return (
     <div className={styles.page}>
       <PageTitle>Catfood</PageTitle>
       <p>Please note that this is a very specific tool for a personal project of mine and will likely make no sense to anyone visiting</p>
-      <PriceObservationForm addEntry={addEntry} options={getOptions(data)} />
-      <PriceObservationList data={data} eraseAll={eraseAll} removeEntry={removeEntry} addEntry={addEntry} />
+      
+      <PriceObservationForm stats={stats} addEntry={addEntry} options={storeOptions} />
+      <PriceObservationList 
+        data={data} stats={stats} 
+        eraseAll={eraseAll} removeEntry={removeEntry} addEntries={addEntries} 
+      />
     </div>
   );
 };
 
 export default Catfood;
-export { DataEntry, Option };
