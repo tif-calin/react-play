23b76e4361c84536df9ecc85c04499ee30d68e40
culi <culitif@tuta.io>
2022-01-05 20:31:02 -0800

    journal log

--- src/views/learn/LearnRuby.tsx
+++ src/views/learn/LearnRuby.tsx
@@ -47,6 +47,15 @@ const LearnRuby = () => {
       <p><strong>2021_12dec23thu</strong>: Did a Exercism problem, learned how to define class methods and properties</p>
       <p><strong>2021_12dec24fri</strong>: Did another Exercism problem. Learned about instantiating. Using ? after getter methods is the cutest thing I&apos;ve ever seen in a language</p>
       <p><strong>2021_12dec25sat</strong>: Did two more Exercism problems. Ruby has a ton of useful built-in string methods. Also rubocop :O</p>
+      <p><strong>2021_12dec30thu</strong>: Did an Exercism problem that taught conditionals and the TwoFer exercise. Getting quite used to the syntax at this point. Everybody says the koans are the best way to learn Ruby but I haven't really gotten a chance to try them out with the holidays and all that. And I've actualyl really been enjoying Exercism so far.</p>
+      <p><strong>2022_01jan02sun</strong>: More Exercism 2 today and 2 yesterday because I'm at a cabin and won't have much time till I get back on Tue/Wed. I want to start the koans soon, but I just got to the point in Exercism where I got passed all the tutorial-y ones and I can work on more complex challenges and can ask for mentoring to get real feedback on my code. I learned some more cool built-in enumeration techniques and also about OpenStruct shortcuts. I also learned about the difference between modules and classes in Ruby. Modules just seem like a way to get the benefits of namespaces without having to use classes</p>
+      <p><strong>2022_01jan03mon</strong>: I started on a problem that was meant to teach error-handling in Ruby yesterday but didn't finish until this morning. Ruby's `unless` keyword comes in handy with error raising logic.</p>
+
+      <h3>Project Plan</h3>
+      <ol>
+        <li>Write RCV algorithm in Ruby</li>
+        <li>Write the backend for a Kipo API in Ruby</li>
+      </ol>
     </div>
   );
 };
