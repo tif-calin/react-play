9755574d27f7f40c0a178a1dd05f5fe4dfef45bc
culi <culitif@tuta.io>
2022-01-06 13:11:34 -0800

    add holidays

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -7,14 +7,17 @@ const Home = () => {
     <>
       <PageTitle>Home</PageTitle>
       <p>
-        Earth day is April 22. Plant a tree, turn your lights off, learn indigenous names for native plants, and build earthen traditions.
+       <b>Groundhog Day</b> is on February 2. March 4-5 is the National <b>Day of Unplugging</b>. <b>Bicycle Day</b> is April 19. <b>Earth Day</b> is April 22. Plant a tree, turn your lights off, learn indigenous names for native plants, and build earthen traditions. <b>Labour Day</b> happens on May 1. Then, on June 19 we have <b>Juneteenth</b>. On October 11, we observe <b>Indigenous People&apos;s Day</b>. Finally, we have <b>New Year's Eve</b> on December 31.
       </p>
       <ul>
         <li>
-          <Link to="/rate">raterate</Link> (WIP)
+          <Link to="/rate">prototype of raterate</Link>
         </li>
         <li>
-          <Link to="/catfood">catfood</Link> (WIP)
+          <Link to="/catfood">catfood price data entry tool</Link>
+        </li>
+        <li>
+          <Link to="/charts">my chart playground</Link>
         </li>
       </ul>
     </>
