6ed0ae8e6098562f1ddee3055d9a4a58fb8e11f0
culi <culitif@tuta.io>
2022-01-06 13:12:59 -0800

    show store chart

--- src/views/catfood/stats/CatfoodStats.tsx
+++ src/views/catfood/stats/CatfoodStats.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import PageTitle from '../../../components/layout/PageTitle';
 import useCatfoodData from '../../../hooks/useCatfood';
+import StoreChart from './StoreChart';
 
 const CatfoodStats: React.FC = () => {
   const { stats } = useCatfoodData();
 
-  const byBrand = Object.entries(stats.brands).map(([brand, stats]: [string, any]) => ({
-    name: brand,
-    storeCount: stats?.stores?.size || 0,
-    foodCount: stats?.foods?.size || 0,
-  })).sort((a, b) => b.storeCount - a.storeCount || b.foodCount - a.foodCount);
+  const byBrand = React.useMemo(
+    () => Object.entries(stats.brands)
+      .map(([brand, stats]: [string, any]) => (
+        {
+          name: brand,
+          storeCount: stats?.stores?.size || 0,
+          foodCount: stats?.foods?.size || 0,
+        }
+      ))
+      .sort((a, b) => b.storeCount - a.storeCount || b.foodCount - a.foodCount)
+    , [stats]
+  );
+
+  const byStore = React.useMemo(() => {
+    const entries = Object.entries(stats.stores);
+
+    return entries.map(([store, stats]: [string, any]) => ({
+      label: store,
+      data: [{ label: store, brandCount: stats.brands.size }]
+    })).sort((a, b) => b.data[0].brandCount - a.data[0].brandCount);
+  }, [stats]);
 
   return (
     <div>
@@ -30,6 +47,7 @@ const CatfoodStats: React.FC = () => {
 
       <section>
         <h3>By store</h3>
+        <StoreChart storeData={byStore} />
       </section>
     </div>
   );
