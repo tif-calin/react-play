463c63c3b39548610210d519a57bc75738314b87
culi <culitif@tuta.io>
2022-01-06 13:12:12 -0800

    add a storechart using react-charts

---
+++ src/views/catfood/stats/StoreChart.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Chart } from 'react-charts';
+
+const Container = styled.div`
+  width: 100%;
+  height: 500px;
+`;
+
+interface Props {
+  storeData: any;
+};
+
+const StoreChart: React.FC<Props> = ({ storeData }) => {
+  // const primaryAxis = React.useMemo(() => {
+  //   return {
+  //     getValue: (data: any) => data.brands,
+  //   }
+  // }, []);
+
+  const primaryAxis = {
+    getValue: (data: any) => data.label,
+  };
+
+  const secondaryAxes = [{
+    getValue: (data: any) => data.brandCount,
+  }];
+
+  const getDatumStyle = () => ({
+    rectangle: {
+      strokeWidth: 12,
+    }
+  });
+
+  return (
+    <Container>
+      <Chart 
+        options={{
+          data: storeData,
+          primaryAxis,
+          secondaryAxes,
+          getDatumStyle
+        }}
+      />
+    </Container>
+  );
+};
+
+export default StoreChart;
