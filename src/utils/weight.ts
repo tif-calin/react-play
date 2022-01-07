e6cb078b0301cc50ff7d5a829b04ca2bf4c1609c
culi <culitif@tuta.io>
2022-01-07 00:21:18 -0800

    add some weight utils

---
+++ src/utils/weight.ts
@@ -0,0 +1,55 @@
+type WeightUnit = 'g' | 'kg' | 'oz' | 'lb';
+
+/**
+ * Convert weight from grams to weight unit with least amount of digits
+ * @param {number} grams - weight in grams
+ * @returns {('g'|'kg'|'oz'|'lb')} - weight unit 
+ */
+const findBestUnit = (grams: number): WeightUnit => {
+  let shortestUnit = 'g' as WeightUnit;
+  let shortestLength = grams.toString().length;
+
+  (['kg', 'oz', 'lb'] as WeightUnit[]).forEach(unit => {
+    const num = convertFromGrams(grams, unit);
+    const len = num.toString().length;
+
+    if (len < shortestLength) {
+      shortestLength = len;
+      shortestUnit = unit;
+    } else if (len === shortestLength) {
+      const lenAfterDot = num.toString().split('.')?.[1]?.length || 0;
+      if (lenAfterDot < grams.toString().split('.')?.[1]?.length || 0) {
+        shortestLength = len;
+        shortestUnit = unit;
+      }
+    }
+  });
+
+  return shortestUnit;
+};
+
+/**
+ * Takes weight in in grams and converts to weight in the given unit
+ * @param weight 
+ * @param unit 
+ * @returns converted weight
+ */
+const convertFromGrams = (weight: number, unit: WeightUnit) => {
+  let newWeight = weight;
+  switch (unit) {
+    case 'kg':
+      newWeight = weight / 1000;
+      break;
+    case 'oz':
+      newWeight = weight / 28.34952312;
+      break;
+    case 'lb':
+      newWeight = weight / 453.59237;
+      break;
+  }
+
+  return Number(newWeight.toFixed(4));
+};
+
+export { findBestUnit, convertFromGrams };
+export type { WeightUnit };
