7eb376e1edef85f49cd2c83613c498a78c873614
culi <culitif@tuta.io>
2022-01-17 00:25:54 -0800

    log

--- src/views/learn/LearnD3.tsx
+++ src/views/learn/LearnD3.tsx
@@ -35,6 +35,22 @@ const ListItem = styled.li`
   }
 `;
 
+const LogEntry = styled.li`
+  width: 100%;
+  font-family: "Inconsolata", monospace;
+  list-style: none;
+  text-indent: -2.5rem;
+
+  & > *:first-child {
+    position: relative;
+    font-weight: 600;
+
+    &::after {
+      content: ': ';
+    }
+  }
+`;
+
 const A = styled(({ children, ...props }: any) => <a target="_blank" rel="noopener noreferrer" {...props} >{children}</a>)`
   transition: color 0.1s;
   color: var(--blue);
@@ -50,52 +66,75 @@ const LearnD3: React.FC<Props> = () => {
       <PageTitle>Learn D3</PageTitle>
       <p>Here are my plans to learn the proper way to use D3 with React. Including my planned projects, learning resources used, and maybe a log.</p>
 
-      <h3>Resources</h3>
-      <ul>
-        <ListItem>
-          <input type="checkbox" defaultChecked disabled />
-          <A href="https://wattenberger.com/blog/d3">Wattenberger&apos;s d3 overview</A>
-          <span>This is a great overview of all the built-in d3 modules by Amelia Wattenberger who made the <A href="https://www.newline.co/fullstack-d3"><i>Full-stack D3 and Data Visualization</i> course</A>.</span>
-        </ListItem>
-        <ListItem>
-          <input type="checkbox" disabled />
-          <A href="https://observablehq.com/">Observable</A>
-          <span>A collection of data visualizations with source code and data for inspiration.</span>
-        </ListItem>
-      </ul>
+      <section>
+        <h3>Resources</h3>
+        <ul>
+          <ListItem>
+            <input type="checkbox" defaultChecked disabled />
+            <A href="https://wattenberger.com/blog/d3">Wattenberger&apos;s d3 overview</A>
+            <span>This is a great overview of all the built-in d3 modules by Amelia Wattenberger who made the <A href="https://www.newline.co/fullstack-d3"><i>Full-stack D3 and Data Visualization</i> course</A>.</span>
+          </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <A href="https://observablehq.com/">Observable</A>
+            <span>A collection of data visualizations with source code and data for inspiration.</span>
+          </ListItem>
+        </ul>
+        <h4>Guides</h4>
+        <ul>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <A href="https://www.pluralsight.com/guides/using-d3.js-inside-a-react-app">PluralSight (2020_09sep)</A>
+            <span>Using D3.js inside of a React app by by Benney Au.</span>
+          </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <A href="https://observablehq.com/@d3/learn-d3?collection=@d3/learn-d3">Observable (2020_03mar)</A>
+            <span>The most popular Observable is a guide to using D3.</span>
+          </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <A href="https://www.freecodecamp.org/news/how-to-get-started-with-d3-and-react-c7da74a5bd9f/">FreeCodeCamp (2019_05may)</A>
+            <span>How to get started with D3 and React by Magdalena Stenius for FreeCodeCamp.</span>
+          </ListItem>
+        </ul>
+      </section>
 
-      <h4>Guides</h4>
-      <ul>
-        <ListItem>
-          <input type="checkbox" disabled />
-          <A href="https://observablehq.com/@d3/learn-d3?collection=@d3/learn-d3">Learn D3 (2020_03mar)</A>
-          <span>The most popular Observable is a guide to using D3.</span>
-        </ListItem>
-      </ul>
+      <section>
+        <h3>Projects</h3>
+        <ul>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <span>Seasonal Fruit Chart</span>
+            <span>A horizontal floating bar chart with an interval x-axis showing when my favorite fruit is in season in Southern California. Data comes from various sources, but often double-checked with the NRDC&apos;s <A href="https://www.seasonalfoodguide.org/">Seasonal Food Guide</A>.</span>
+          </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <span>US Federal Judges Partisan Data Visualization</span>
+            <span>It&apos;s surprisingly hard to find a data visualization of US federal judges based on the party of the president that elected them. I guess the myth of judicial independence runs deep. Anyways, I&apos;d like to make one~</span>
+          </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <span>Dynamic Cladistogram</span>
+            <span>Give it a Newick string, and it&apos;s generated a fancy, collapsible tree diagram like <A href="https://observablehq.com/@d3/collapsible-tree?collection=@observablehq/visualization">this one</A>.</span>
+          </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <span>Coyote Watch Index</span>
+            <span>Could go a lot of directions with this. Basically, I want a data visualization showing the number of coyote sightings in an area based on iNaturalist observation data. Perhaps it could even take in a location, fetch the data dynamically, and then produce the graphics?</span>
+          </ListItem>
+        </ul>
+      </section>
 
-      <h3>Projects</h3>
-      <ul>
-        <ListItem>
-          <input type="checkbox" disabled />
-          <span>Seasonal Fruit Chart</span>
-          <span>A horizontal floating bar chart with an interval x-axis showing when my favorite fruit is in season in Southern California. Data comes from various sources, but often double-checked with the NRDC&apos;s <A href="https://www.seasonalfoodguide.org/">Seasonal Food Guide</A>.</span>
-        </ListItem>
-        <ListItem>
-          <input type="checkbox" disabled />
-          <span>US Federal Judges Partisan Data Visualization</span>
-          <span>It&apos;s surprisingly hard to find a data visualization of US federal judges based on the party of the president that elected them. I guess the myth of judicial independence runs deep. Anyways, I&apos;d like to make one~</span>
-        </ListItem>
-        <ListItem>
-          <input type="checkbox" disabled />
-          <span>Dynamic Cladistogram</span>
-          <span>Give it a Newick string, and it&apos;s generated a fancy, collapsible tree diagram like <A href="https://observablehq.com/@d3/collapsible-tree?collection=@observablehq/visualization">this one</A>.</span>
-        </ListItem>
-        <ListItem>
-          <input type="checkbox" disabled />
-          <span>Coyote Watch Index</span>
-          <span>Could go a lot of directions with this. Basically, I want a data visualization showing the number of coyote sightings in an area based on iNaturalist observation data. Perhaps it could even take in a location, fetch the data dynamically, and then produce the graphics?</span>
-        </ListItem>
-      </ul>
+      <section>
+        <h3>Log</h3>
+        <ul>
+          <LogEntry>
+            <span>2022_01jan16</span>
+            <span>Today I created this page. I collected a lot of the tabs I had open into a list of resources, wrote down a lot of ideas I had floating around that could make good practice projects for d3, and I started doing more research into d3/react. I also started reading this <A href="https://www.pluralsight.com/guides/using-d3.js-inside-a-react-app">PluralSight guide</A>. Main takeaways from that: use refs and to avoid .append and instead use selection joins.</span>
+          </LogEntry>
+        </ul>
+      </section>
     </Container>
   );
 };
