1f55cd005dc09a2adb2f92cf75aabb863c9c9cb7
culi <culitif@tuta.io>
2022-01-17 12:20:55 -0800

    update learnd3 page

--- src/views/learn/LearnD3.tsx
+++ src/views/learn/LearnD3.tsx
@@ -70,20 +70,20 @@ const LearnD3: React.FC<Props> = () => {
         <h3>Resources</h3>
         <ul>
           <ListItem>
-            <input type="checkbox" defaultChecked disabled />
+            <input type="checkbox" disabled defaultChecked />
             <A href="https://wattenberger.com/blog/d3">Wattenberger&apos;s d3 overview</A>
             <span>This is a great overview of all the built-in d3 modules by Amelia Wattenberger who made the <A href="https://www.newline.co/fullstack-d3"><i>Full-stack D3 and Data Visualization</i> course</A>.</span>
           </ListItem>
           <ListItem>
-            <input type="checkbox" disabled />
+            <input type="checkbox" disabled defaultChecked />
             <A href="https://observablehq.com/">Observable</A>
-            <span>A collection of data visualizations with source code and data for inspiration.</span>
+            <span>A collection of data visualizations with source code and data for inspiration. Check out D3&apos;s <A href="https://observablehq.com/@d3">Observable page</A> also.</span>
           </ListItem>
         </ul>
         <h4>Guides</h4>
         <ul>
           <ListItem>
-            <input type="checkbox" disabled />
+            <input type="checkbox" disabled defaultChecked />
             <A href="https://www.pluralsight.com/guides/using-d3.js-inside-a-react-app">PluralSight (2020_09sep)</A>
             <span>Using D3.js inside of a React app by by Benney Au.</span>
           </ListItem>
@@ -104,9 +104,14 @@ const LearnD3: React.FC<Props> = () => {
         <h3>Projects</h3>
         <ul>
           <ListItem>
-            <input type="checkbox" disabled />
+            <input type="checkbox" disabled defaultChecked />
             <span>Seasonal Fruit Chart</span>
             <span>A horizontal floating bar chart with an interval x-axis showing when my favorite fruit is in season in Southern California. Data comes from various sources, but often double-checked with the NRDC&apos;s <A href="https://www.seasonalfoodguide.org/">Seasonal Food Guide</A>.</span>
+          </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <span>Ranked Choice Voting Animated Bar Chart</span>
+            <span>Feed it some data showing the results of an RCV election at each round and it should generate a bar chart that will loop through the rounds.</span>
           </ListItem>
           <ListItem>
             <input type="checkbox" disabled />
@@ -131,7 +136,7 @@ const LearnD3: React.FC<Props> = () => {
         <ul>
           <LogEntry>
             <span>2022_01jan16</span>
-            <span>Today I created this page. I collected a lot of the tabs I had open into a list of resources, wrote down a lot of ideas I had floating around that could make good practice projects for d3, and I started doing more research into d3/react. I also started reading this <A href="https://www.pluralsight.com/guides/using-d3.js-inside-a-react-app">PluralSight guide</A>. Main takeaways from that: use refs and to avoid .append and instead use selection joins.</span>
+            <span>Today I created this page. I collected a lot of the tabs I had open into a list of resources, wrote down a lot of ideas I had floating around that could make good practice projects for d3, and I started doing more research into d3/react. I also started reading this <A href="https://www.pluralsight.com/guides/using-d3.js-inside-a-react-app">PluralSight guide</A>. Main takeaways from that: use refs and to avoid .append and instead use selection joins. I used it to build the seasonal fruit chart, though it needs a lot of work.</span>
           </LogEntry>
         </ul>
       </section>
