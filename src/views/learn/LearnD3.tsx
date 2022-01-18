e84e709dbc18b1cd32c93ce5fad015d8a9495578
culi <culitif@tuta.io>
2022-01-18 15:20:33 -0800

    log for today + yesterday

--- src/views/learn/LearnD3.tsx
+++ src/views/learn/LearnD3.tsx
@@ -79,6 +79,11 @@ const LearnD3: React.FC<Props> = () => {
             <A href="https://observablehq.com/">Observable</A>
             <span>A collection of data visualizations with source code and data for inspiration. Check out D3&apos;s <A href="https://observablehq.com/@d3">Observable page</A> also.</span>
           </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <A href="https://www.d3indepth.com/">D3 In Depth</A>
+            <span>A lot of good guides here on different aspects of d3.</span>
+          </ListItem>
         </ul>
         <h4>Guides</h4>
         <ul>
@@ -97,6 +102,16 @@ const LearnD3: React.FC<Props> = () => {
             <A href="https://www.freecodecamp.org/news/how-to-get-started-with-d3-and-react-c7da74a5bd9f/">FreeCodeCamp (2019_05may)</A>
             <span>How to get started with D3 and React by Magdalena Stenius for FreeCodeCamp.</span>
           </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <A href="https://betterprogramming.pub/d3-animations-in-react-with-1-line-of-code-976396a45ede">David Mora (2020_03mar)</A>
+            <span>How to create D3 animations in React.</span>
+          </ListItem>
+          <ListItem>
+            <input type="checkbox" disabled />
+            <A href="https://medium.com/@varvara.munday/d3-in-react-a-step-by-step-tutorial-cba33ce000ce">Varvara Munday (2020_04apr)</A>
+            <span>D3 in React: A step-by-step tutorial</span>
+          </ListItem>
         </ul>
       </section>
 
@@ -109,7 +124,7 @@ const LearnD3: React.FC<Props> = () => {
             <span>A horizontal floating bar chart with an interval x-axis showing when my favorite fruit is in season in Southern California. Data comes from various sources, but often double-checked with the NRDC&apos;s <A href="https://www.seasonalfoodguide.org/">Seasonal Food Guide</A>.</span>
           </ListItem>
           <ListItem>
-            <input type="checkbox" disabled />
+            <input type="checkbox" disabled defaultChecked />
             <span>Ranked Choice Voting Animated Bar Chart</span>
             <span>Feed it some data showing the results of an RCV election at each round and it should generate a bar chart that will loop through the rounds.</span>
           </ListItem>
@@ -135,8 +150,20 @@ const LearnD3: React.FC<Props> = () => {
         <h3>Log</h3>
         <ul>
           <LogEntry>
-            <span>2022_01jan16</span>
+            <span>2022_01jan16sun</span>
             <span>Today I created this page. I collected a lot of the tabs I had open into a list of resources, wrote down a lot of ideas I had floating around that could make good practice projects for d3, and I started doing more research into d3/react. I also started reading this <A href="https://www.pluralsight.com/guides/using-d3.js-inside-a-react-app">PluralSight guide</A>. Main takeaways from that: use refs and to avoid .append and instead use selection joins. I used it to build the seasonal fruit chart, though it needs a lot of work.</span>
+          </LogEntry>
+          <LogEntry>
+            <span>2022_01jan17mon</span>
+            <span>Today I built another bar graph and this time it&apos;s animated! The page has a textarea for inputting a json-formatted string as an array of objects. You can modify the string while it&apos;s running and it&apos;ll really smoothly figure it out. The way it basically works is there&apos;s an interval timer that counts up and transitions to the next round of results every 3 seconds. When this happens, the render function gets triggered again. If the current round number is 0, it&apos;ll render everything all at once, otherwise it&apos;ll simply do a .transition() to smoothly change the height of the bar graphs. There&apos;s still quite a few optimizations that can be made though. I&apos;m pretty sure there&apos;s a more specific way to make updates to a chart though.
+            <br/><br/>
+            Today I also watched two episodes of <A href="https://www.youtube.com/playlist?list=PLzCAqE_rafAc_2QWK8ii16m2ju4qhYAJT">this d3+react+typescript series</A> on youtube</span>
+          </LogEntry>
+          <LogEntry>
+            <span>2020_01jan18tue</span>
+            <span>
+              Today I added labels to the RCV animated bar chart and read a tutorial for d3 in react by Varvara Munday. The article made me realize the ways I wasn&apos;t taking advantage of D3&apos;s .join functionality. When a data element is joined, the <i>enter</i> selection gets triggered when a new piece of data is added and the <i>exit</i> selection is triggered when one of those datums is removed. 
+            </span>
           </LogEntry>
         </ul>
       </section>
