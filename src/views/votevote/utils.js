c4a73d4ce62c4c8bc50888ea4d16f979698c07da
culi <culitif@tuta.io>
2022-01-20 12:27:16 -0800

    add 2 more rcv variations

--- src/views/votevote/utils.js
+++ src/views/votevote/utils.js
@@ -2,15 +2,89 @@
 import colors from '../../data/colors.js';
 
 /**
- * Ranked Choice Voting algorithm
- * @param {Array.<string>} candidates 
- * @param {Array.<Array.<string>>} votes 
+ * Generalized RCV or Coombs Method
+ * @param {Array.<string>} candidates
+ * @param {Array.<Array.<string>>} votes
+ * @param {'rcv' | 'coombs' | 'culi'} method
  * @returns {Array.<Round>}
  * 
  * @typedef {Object} Round 
  * @property {number} color
  */
-const rankedChoiceVote = (candidates, votes) => {
+const generalizedRCV = (candidates, votes, method = 'rcv') => {
+  const rounds = [];
+
+  const dropped = [];
+  let isOver = false;
+
+  while (!isOver) {
+    // initialize round with all candidates set to 0
+    const firstPicks = candidates.filter(c => !dropped.includes(c)).reduce((acc, c) => ({ ...acc, [c]: 0 }), {});
+    const lastPicks = method === 'rcv' ? {} : candidates.filter(c => !dropped.includes(c)).reduce((acc, c) => ({ ...acc, [c]: 0 }), {});
+    let combined = {};
+
+    // loop through votes and find first undropped candidate
+    votes.forEach(vote => {
+      const firstUndroppedPick = vote.find(candidate => !dropped.includes(candidate));
+      if (firstUndroppedPick) firstPicks[firstUndroppedPick]++;
+    });
+
+    // loop through votes and find the last undropped candidate
+    if (method !== 'rcv') votes.forEach(vote => {
+      const lastUndroppedPick = [...vote].reverse().find(candidate => !dropped.includes(candidate));
+      if (lastUndroppedPick) lastPicks[lastUndroppedPick]++;
+    });
+
+    // loop though and find the combined score
+    if (method === 'culi') combined = candidates.reduce((acc, candidate) => ({
+      ...acc,
+      [candidate]: firstPicks[candidate] - lastPicks[candidate]
+    }), {});
+
+    // drop the lowest scoring candidates
+    const scores = {
+      rcv: firstPicks,
+      coombs: lastPicks,
+      culi: combined
+    }[method];
+    const dropScore = (method === 'coombs' ? Math.max : Math.min)(...Object.values(scores));
+    Object.keys(scores).forEach(key => {
+      if (scores[key] === dropScore) dropped.push(key);
+    });
+
+    // check the isOver conditions: (1) all remaining are tied; (2) someone has more than 50% of the (remaining) votes
+    if (Object.values(scores).every(
+      val => val === Object.values(scores)[0]
+    )) isOver = true;
+    else {
+      const topScore = Math.max(...Object.values(firstPicks));
+      const totScore = Object.values(firstPicks).reduce((acc, val) => acc + val, 0);
+      if (topScore > (totScore / 2)) isOver = true;
+    }
+
+    // record this round
+    rounds.push(firstPicks);
+
+    console.table(firstPicks);
+    console.log(dropScore, dropped, lastPicks);
+    if (rounds.length > candidates.length) {
+      console.error(`ERROR: more rounds than candidates: Method ${method}`);
+      console.table(rounds);
+      break;
+    }
+  }
+
+  return rounds;
+};
+
+/**
+ * Ranked Choice Voting algorithm
+ * @param {Array.<string>} candidates 
+ * @param {Array.<Array.<string>>} votes 
+ * @returns {Array.<Round>}
+ */
+const rankedChoiceVote = (candidates, votes) => generalizedRCV(candidates, votes, 'rcv');
+const rankedChoiceVoteOld = (candidates, votes) => {
   // to keep track of every round
   const results = [];
 
@@ -61,6 +135,22 @@ const rankedChoiceVote = (candidates, votes) => {
 };
 
 /**
+ * Coomb's RCV Method
+ * @param {Array.<string>} candidates 
+ * @param {Array.<Array.<string>>} votes 
+ * @returns @returns {Array.<Round>}
+ */
+const coombsRCV = (candidates, votes) => generalizedRCV(candidates, votes, 'coombs');
+
+/**
+ * Culi's RCV Method
+ * @param {Array.<string>} candidates 
+ * @param {Array.<Array.<string>>} votes 
+ * @returns @returns {Array.<Round>}
+ */
+const culiRCV = (candidates, votes) => generalizedRCV(candidates, votes, 'culi');
+
+/**
  * Takes a string as a hex color and returns an array of the RGB values
  * @param {string} hex 
  * @returns {Array.<number>}
@@ -68,7 +158,10 @@ const rankedChoiceVote = (candidates, votes) => {
 const hex2rgb = hex => /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex).map(str => parseInt(str, 16)).slice(1);
 
 /**
- * 
+ * Sorts a list of candidate by their distance (in RGB) from a given color
+ * @param {string} color
+ * @param {Array.<string>} candidates
+ * @returns {Array.<string>}
  */
 const calcBallotPreferences = (voter, candidates) => {
   const [R, G, B] = hex2rgb(colors[voter]);
@@ -80,4 +173,4 @@ const calcBallotPreferences = (voter, candidates) => {
   ).sort((a, b) => a[1] - b[1]).map(arr => arr[0]);
 };
 
-export { rankedChoiceVote, calcBallotPreferences };
+export { rankedChoiceVote, coombsRCV, culiRCV, calcBallotPreferences };
