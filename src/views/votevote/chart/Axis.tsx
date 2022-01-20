917e7e2ab85dd2f3f9790c30a9cba27f97a3ebef
culi <culitif@tuta.io>
2022-01-19 21:52:47 -0800

    skeleton for axis and bar components

---
+++ src/views/votevote/chart/Axis.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+  domain: [number, number];
+  range: [number, number];
+};
+
+const Axis: React.FC<Props> = ({ domain, range }) => {
+  return (
+    <div></div>
+  );
+};
+
+export default Axis;
