1bc0c2c1897585fc252e6c5891c8c567a6a327e8
culi <culitif@tuta.io>
2022-01-23 20:46:15 -0800

    alternative vote, plurality, borda, approval

--- src/views/votevote/utils/votingMethods.js
+++ src/views/votevote/utils/votingMethods.js
@@ -1,21 +1,21 @@
 // @ts-nocheck
 
+// #region IRV 
 /**
- * Generalized RCV or Coombs Method
+ * Generalized IRV Method
  * @param {Array.<string>} candidates
  * @param {Array.<Array.<string>>} votes
  * @param {'rcv' | 'coombs' | 'culi'} method
  * @returns {Array.<Round>}
  * 
  * @typedef {Object} Round 
- * @property {number} color
+ * @property {number} candidate - how many votes a candidate got that round
  */
 const generalizedRCV = (candidates, votes, method = 'rcv') => {
   const rounds = [];
 
   const dropped = [];
   let isOver = false;
-
   while (!isOver) {
     // initialize round with all candidates set to 0
     const firstPicks = candidates.filter(c => !dropped.includes(c)).reduce((acc, c) => ({ ...acc, [c]: 0 }), {});
@@ -31,13 +31,13 @@ const generalizedRCV = (candidates, votes, method = 'rcv') => {
     // loop through votes and find the last undropped candidate
     if (method !== 'rcv') votes.forEach(vote => {
       const lastUndroppedPick = [...vote].reverse().find(candidate => !dropped.includes(candidate));
-      if (lastUndroppedPick) lastPicks[lastUndroppedPick]++;
+      if (lastUndroppedPick) lastPicks[lastUndroppedPick]--;
     });
 
     // loop though and find the combined score
     if (method === 'culi') combined = candidates.filter(c => !dropped.includes(c)).reduce((acc, candidate) => ({
       ...acc,
-      [candidate]: firstPicks[candidate] - lastPicks[candidate]
+      [candidate]: firstPicks[candidate] + lastPicks[candidate]
     }), {});
 
     // drop the lowest scoring candidates
@@ -46,7 +46,7 @@ const generalizedRCV = (candidates, votes, method = 'rcv') => {
       coombs: lastPicks,
       culi: combined
     }[method];
-    const dropScore = (method === 'coombs' ? Math.max : Math.min)(...Object.values(scores));
+    const dropScore = Math.min(...Object.values(scores));
     Object.keys(scores).forEach(key => {
       if (scores[key] === dropScore) dropped.push(key);
     });
@@ -87,7 +87,7 @@ const rankedChoiceVote = (candidates, votes) => generalizedRCV(candidates, votes
  * Coomb's RCV Method
  * @param {Array.<string>} candidates 
  * @param {Array.<Array.<string>>} votes 
- * @returns @returns {Array.<Round>}
+ * @returns {Array.<Round>}
  */
 const coombsRCV = (candidates, votes) => generalizedRCV(candidates, votes, 'coombs');
 
@@ -95,8 +95,176 @@ const coombsRCV = (candidates, votes) => generalizedRCV(candidates, votes, 'coom
  * Culi's RCV Method
  * @param {Array.<string>} candidates 
  * @param {Array.<Array.<string>>} votes 
- * @returns @returns {Array.<Round>}
+ * @returns {Array.<Round>}
  */
 const culiRCV = (candidates, votes) => generalizedRCV(candidates, votes, 'culi');
 
-export { rankedChoiceVote, coombsRCV, culiRCV };
+// #endregion 
+
+// #region supplementary
+/**
+ * Supplementary Method
+ * @param {Array.<string>} candidates
+ * @param {Array.<Array.<string>>} votes
+ * @returns {Array.<Round>}
+ */
+const supplementary = (candidates, votes) => {
+  const munged = votes
+    .map(vote => vote.filter(c => candidates.includes(c)))
+    .filter(vote => vote.length)
+  ;
+
+  const fptp = munged.map(v => v[0]).reduce((acc, vote) => ({
+    ...acc,
+    [vote]: ~~acc[vote] + 1
+  }), {});
+
+  // check if top vote-getter has majority
+  const threshold = Math.floor(munged.length / 2);
+  const topScore = Math.max(...Object.values(fptp));
+  if (topScore > threshold) return [fptp];
+
+  // otherwise, find all with the top vote.
+  const runoffCandidates = candidates.filter(c => fptp[c] === topScore);
+  if (runoffCandidates.length < 2) {
+    // find the second highest score
+    const secondTopScore = Math.max(...Object.values(fptp).filter(val => val !== topScore));
+
+    // find all candidates with the second highest score
+    candidates.filter(c => fptp[c] === secondTopScore).forEach(c => runoffCandidates.push(c));
+  }
+
+  const result = {};
+
+  munged.forEach(vote => {
+    const pick = vote.find(c => runoffCandidates.includes(c));
+    if (pick) result[pick] = ~~result[pick] + 1;
+  });
+
+  return [fptp, result];
+};
+
+// #endregion
+
+// #region plurality
+/**
+ * First Past the Post Method
+ * @param {Array.<string>} votes 
+ * @returns {Round}
+ */
+const fptp = (votes) => {
+  return votes.reduce((acc, vote) => ({
+    ...acc,
+    [vote]: ~~acc[vote] + 1
+  }), {});
+};
+
+// #endregion
+
+// #region borda
+/**
+ * Borda Count Method
+ * @param {Array.<string>} candidates
+ * @param {Array.<Array.<string>>} votes
+ * @returns {Round}
+ */
+const borda = (candidates, votes) => {
+  const result = {};
+
+  // munge data: remove duplicates and non-candidates
+  const mungedVotes = votes.map(vote => vote.filter(
+    (candidate, i) => candidates.includes(candidate) && vote.indexOf(candidate) === i
+  ));
+
+  mungedVotes.forEach(vote => {
+    // let bordaScore = candidates.length - vote.length;
+    // [...vote].reverse().forEach(candidate => result[candidate] = ~~result[candidate] + bordaScore++);
+    let bordaScore = candidates.length;
+    vote.forEach(candidate => {
+      result[candidate] = ~~result[candidate] + --bordaScore;
+    })
+  })
+
+  return result;
+};
+
+/** 
+ * Modified Borda Count also allows for ties at the bottom of the ranking, but unranked candidates get 0
+ * @param {Array.<string>} candidates
+ * @param {Array.<Array.<string>>} votes
+ * @returns {Round}
+ */
+const modifiedBorda = (candidates, votes) => {
+  const result = {};
+
+  const mungedVotes = votes.map(vote => vote.filter(
+    (c, i) => candidates.includes(c) && vote.indexOf(c) === i)
+  );
+
+  mungedVotes.forEach(vote => {
+    let bordaScore = vote.length === candidates.length ? 0 : 1;
+    [...vote].reverse().forEach(c => result[c] = ~~result[c] + bordaScore++);
+  });
+
+  return result;
+};
+
+/**
+ * Allows for ties anywhere in the ranking
+ * @param {Array.<string>} candidates 
+ * @param {Array.<Array.<string|Array.<string>>>} votes 
+ * @returns {Round}
+ */
+const tournamentBorda = (candidates, votes) => {
+  const result = {};
+
+  const mungedVotes = votes.map(vote => {
+    vote = vote.map(c => [c].flat().filter(x => candidates.includes(x)));
+    const flattenedBallot = vote.flat();
+
+    return vote.map((cArr, i) => {
+      return cArr.filter((c, j) => (i+j) === flattenedBallot.indexOf(c));
+    }).filter(cArr => cArr.length);
+  });
+
+  mungedVotes.forEach(vote => {
+    let bordaScore = candidates.length;
+
+    vote.forEach(arr => {
+      bordaScore -= arr.length;
+      const score = bordaScore + (arr.length - 1) / 2;
+      arr.forEach(c => result[c] = ~~result[c] + score);
+    });
+  });
+
+  return result;
+};
+
+const dowdallBorda = (candidates, votes) => {
+
+};
+// #endregion
+
+// #region approval
+/**
+ * Approval Method
+ * @param {Array.<Array.<string>>} votes 
+ * @returns {Round}
+ */
+const approval = votes => {
+  return votes.flat().reduce((acc, candidate) => {
+    return {
+      ...acc,
+      [candidate]: ~~acc[candidate] + 1
+    };
+  }, {});
+};
+// #endregion
+
+export { 
+  rankedChoiceVote, coombsRCV, culiRCV, 
+  supplementary,
+  fptp, 
+  borda, modifiedBorda, tournamentBorda,
+  approval
+};
