22f1c0bfc890913957ee819142ffa667693c1107
culi <culitif@tuta.io>
2022-01-27 13:39:26 -0800

    moar pretty

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import RainbowText from '../RainbowText';
 
 const Header = () => {
   return (
     <header className="Header">
-      <Link to="/"><h1>Culi's Prototype Playground</h1></Link>
+      {/* <Link to="/"><h1>Culi's Prototype Playground</h1></Link> */}
+      <Link to="/"><h1><RainbowText text="Culi's Prototype Playground" /></h1></Link>
     </header>
   );
 };
