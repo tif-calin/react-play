9c7f466cf01e0ded1607a7af469373f355c9953f
culi <culitif@tuta.io>
2022-01-27 15:40:44 -0800

    convenient component

---
+++ src/components/A.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const A = styled(({ children, ...props }: any) => <a target="_blank" rel="noopener noreferrer" {...props} >{children}</a>)`
+  transition: color 0.1s;
+  color: var(--blue);
+
+  &:hover { color: unset }
+`;
+
+export default A;
