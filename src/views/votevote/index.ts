827c2575d76ae22f24d7888f6a5fb577c54adb8b
culi <culitif@tuta.io>
2022-01-27 21:39:31 -0800

    notes for votevote

--- src/views/votevote/index.ts
+++ src/views/votevote/index.ts
@@ -5,3 +5,12 @@ type ColorName = keyof typeof colors;
 
 export default VoteVotePage;
 export type { ColorName };
+
+/*
+
+NOTES FOR WHEN I MAKE VOTEVOTE.PAGE 
+ • build out each voting method to take into account weight by default
+ • instead of a common BarChart component, each method should get its own
+ • save the colors as objects with hexes, rgb, and hsl so you don't have to convert each time
+
+*/
