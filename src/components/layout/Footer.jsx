a25a99df4ebe40c7aa369e689cd12e422c67caa3
culi <culitif@tuta.io>
2022-01-29 15:59:07 -0800

    skull footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="Footer">
-      <a href="https://github.com/tif-calin/react-play">steal this</a>
+      <a href="https://github.com/tif-calin/react-play">☠ steal this ☠</a>
     </footer>
   );
 };
