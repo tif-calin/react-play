b271788159d4c24d0ff20c67210547cf010ba2dc
Culi Tif <calin.tif@justvanilla.com>
2022-03-04 14:11:33 -0800

    update layout

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="Header">
       {/* <Link to="/"><h1>Culi's Prototype Playground</h1></Link> */}
-      <Link to="/"><h1><RainbowText text="Culi's Prototype Playground" /></h1></Link>
+      <Link to="/"><span className="site-title"><RainbowText text="Culi's Prototype Playground" /></span></Link>
     </header>
   );
 };
--- src/components/layout/PageTitle.tsx
+++ src/components/layout/PageTitle.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const PageTitle: React.FC<any> = ({ children }) => {
   return (
-    <h2 className="PageTitle">
+    <h1 className="PageTitle">
       {children}
-    </h2>
+    </h1>
   );
 };
 
