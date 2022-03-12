3452c91dae6c201ca5b1f957d35c630830ee98ef
Culi Tif <calin.tif@justvanilla.com>
2022-03-12 14:42:54 -0800

    update homepage

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import A from '../../components/A';
 import PageTitle from '../../components/layout/PageTitle';
 
 const StyledList = styled.ul`
@@ -23,18 +24,23 @@ const Home = () => {
        <b>Groundhog Day</b> is on February 2. March 4-5 is the National <b>Day of Unplugging</b>. <b>Bicycle Day</b> is April 19. <b>Earth Day</b> is April 22. Plant a tree, turn your lights off, learn indigenous names for native plants, and build earthen traditions. <b>Labour Day</b> happens on May 1. Then, on June 19 we have <b>Juneteenth</b>. On October 11, we observe <b>Indigenous People&apos;s Day</b>. Finally, we have <b>New Year's Eve</b> on December 31.
       </p>
       <StyledList>
-        <li>
+        <li><Link to="redistricting2020">My measurement of how gerrymandered new districting maps are</Link>.</li>
+        <li><Link to="rate/multi">My experiments with budgetory sliders.</Link></li>
+        <li><Link to="charts">I make charts sometimes</Link></li>
+        <li><Link to="votevote">The original prototype of VoteVote</Link>. Which has now become a seperate website: <A href="https://votevote.page">votevote.page</A></li>
+        <li><Link to="learn">My collection of learning resource aggregators</Link>.</li>
+        {/* <li>
           React forms are still hard tbh, so I tried playing around with a couple things: <Link to="/rate">prototype of raterate</Link>, <Link to="/catfood">catfood price data entry tool</Link> and their <Link to="/catfood/stats">stats</Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           <Link to="/charts">my chart playground</Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           Sometimes I log my learning journeys like with <Link to="/learn/d3">d3</Link> or <Link to="/learn/ruby">ruby</Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           The coolest thing I&apos;ve made so far is my prototype of <Link to="/votevote">votevote.page</Link> (soon to be a <a href="https://votevote.page/">website</a>).
-        </li>
+        </li> */}
       </StyledList>
     </>
   )
