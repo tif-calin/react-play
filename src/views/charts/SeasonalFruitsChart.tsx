40498084b5d015ca2ef7d3e32c7632b1a75a2c88
Culi Tif <calin.tif@justvanilla.com>
2022-04-02 14:40:41 -0700

    moar froot

--- src/views/charts/SeasonalFruitsChart.tsx
+++ src/views/charts/SeasonalFruitsChart.tsx
@@ -13,15 +13,24 @@ const useD3 = (renderChart: any, dependencies: any[]) => {
 
 const data = {
   'asian pears': [9.25, 11.75],
+  'blackberries': [6, 9],
   'blood oranges': [12, 4],
   'cherimoya': [11, 6.75],
   'cherries': [4.5, 6.25],
-  'figs': [9, 12],
+  'figs': [8, 11],
+  'gooseberries': [6, 9],
+  'grapes': [6, 12],
+  'kiwi': [10, 5],
+  'longan': [7, 11],
+  'loquats': [4, 7],
   'lychees': [8.5, 9.5],
   'mandarins': [11, 4],
   'mangos': [6, 9],
   'passionfruits': [1, 10],
-  'persimmons': [9.75, 1]
+  'pears': [8, 11],
+  'persimmons': [9.75, 1],
+  'raspberries': [4, 11],
+  'strawberries': [2, 11],
 };
 
 const toMonth = (i: d3.NumberValue) => [
@@ -39,6 +48,22 @@ const colors = [
 ];
 
 const today = 1 + new Date().getMonth() + new Date().getDate() / 31;
+
+const dataWithWrappedBars = Object.entries(data).map(arr => arr.flat()).reduce(
+  (acc: any, curr: any) => {
+    if (curr.at(-1) > curr.at(1)) return [...acc, curr];
+    else return [
+      ...acc,
+      [curr.at(0), curr.at(1), 12.999],
+      [curr.at(0), 1, curr.at(2)]
+    ];
+  }, []
+);
+
+const inSeason = new Set(dataWithWrappedBars
+  .filter(([_, m1, m2]) => m1 <= today && today <= m2)
+  .map(d => d[0])
+);
 
 const renderChart = (svg: any) => {
   const margin = { top: 30, right: 0, bottom: 30, left: 100 };
@@ -71,19 +96,9 @@ const renderChart = (svg: any) => {
   svg.select('.x-axis').call(xAxis);
   svg.select('.y-axis').call(yAxis);
 
-  const dataWithWrappedBars = Object.entries(data).map(arr => arr.flat()).reduce(
-    (acc: any, curr: any) => {
-      if (curr.at(-1) > curr.at(1)) return [...acc, curr];
-      else return [
-        ...acc,
-        [curr.at(0), curr.at(1), 12.999],
-        [curr.at(0), 1, curr.at(2)]
-      ];
-    }, []
-  );
-
   const fruits = Object.keys(data);
 
+  // bars
   svg
     .select('.plot-area')
     .selectAll('.bar')
@@ -91,6 +106,7 @@ const renderChart = (svg: any) => {
     .join('rect')
     .attr('class', 'bar')
     .attr('fill', (d: any) => `var(--${colors[fruits.indexOf(d[0]) * 5 % colors.length]})`)
+    .attr('opacity', (d: any) => inSeason.has(d[0]) ? '1' : '0.2')
     .attr('x', (d: any[]) => xScale(d[1]))
     .attr('width', (d: any) => Math.abs(xScale(d[2]) - xScale(d[1])))
     .attr('y', (d: any) => margin.top + (fruits.indexOf(d[0]) + 1/8) * yScale.bandwidth())
@@ -100,6 +116,7 @@ const renderChart = (svg: any) => {
     .text((d: any) => `${d[0]}\n${toFancyMonth((data[d[0] as keyof typeof data])[0])} - ${toFancyMonth(data[d[0] as keyof typeof data][1])}`)
   ;
 
+  // today marker
   svg
     .select('.plot-area')
     .selectAll('.today')
@@ -111,7 +128,6 @@ const renderChart = (svg: any) => {
     .attr('y', margin.top)
     .attr('height', height - margin.top - margin.bottom)
   ;
-
 };
 
 interface Props {};
