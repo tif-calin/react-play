d72412c74b8c8c5d17b9556e7ee318589f9d4125
culi🌸 <culitif@tuta.io>
2022-07-18 20:08:01 -0700

    readlater homepage link

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -24,6 +24,7 @@ const Home = () => {
        <b>Groundhog Day</b> is on February 2. March 4-5 is the National <b>Day of Unplugging</b>. <b>Bicycle Day</b> is April 19. <b>Earth Day</b> is April 22. Plant a tree, turn your lights off, learn indigenous names for native plants, and build earthen traditions. <b>Labour Day</b> happens on May 1. Then, on June 19 we have <b>Juneteenth</b>. On October 11, we observe <b>Indigenous People&apos;s Day</b>. Finally, we have <b>New Year's Eve</b> on December 31.
       </p>
       <StyledList>
+        <li>My tool to help me sort my articles to <Link to="readlater">read later</Link></li>
         <li><Link to="redistricting2020">My measurement of how gerrymandered new districting maps are</Link>. Also relevant is my <Link to="partisanlean">graphs of change in state partisan lean over time</Link></li>
         <li><Link to="rate/multi">My experiments with budgetory sliders</Link>.</li>
         <li><Link to="charts">I make charts sometimes</Link>.</li>
